29564bd737d2747dc9044af098ef79a9b6828437
kharioki <antolisah@gmail.com>
2020-07-30 22:34:21 +0300

    updated title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
+      <h1>KikiGram</h1>
+      <h2>My Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
-  )
-}
+  );
+};
 
 export default Title;
