9d6074f95054a6adc7a787a27f3c678c66aca023
kharioki <antolisah@gmail.com>
2020-07-31 21:47:40 +0300

    added framer animation to progress bar, modal

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
+import { motion } from 'framer-motion';
 
 const ImageGrid = ({ setSelectedImage }) => {
   const { docs } = useFirestore('images');
@@ -8,13 +9,20 @@ const ImageGrid = ({ setSelectedImage }) => {
     <div className="img-grid">
       {docs &&
         docs.map(doc => (
-          <div
+          <motion.div
+            layout
+            whileHover={{ opacity: 1 }}
             className="img-wrap"
             key={doc.id}
             onClick={() => setSelectedImage(doc.url)}
           >
-            <img src={doc.url} alt="uploaded pic" />
-          </div>
+            <motion.img 
+              src={doc.url} 
+              alt="uploaded pic" 
+              initial={{ opacity: 0 }} 
+              animate={{ opacity: 1 }} 
+              transition={{ delay: 1 }} />
+          </motion.div>
         ))}
     </div>
   );
